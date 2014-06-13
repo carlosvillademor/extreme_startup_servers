e1d259fd6b3232270e083804f078360cde2e1328
Carlos Fernandez Garcia <carlos.villademor@gmail.com>
2014-06-13 15:53:48 +0100

    adding double plus and starting fibonnaci

--- javascript/express/extreme_startup.js
+++ javascript/express/extreme_startup.js
@@ -3,10 +3,13 @@ var _ = require('lodash');
 
 /* Reimplement this function to answer questions. */
 var answer = function(question, req, res) {
+
     var largestMatch = question.match(/which of the following numbers is the largest:(.*)/);
     var bananaColourMatch = question.match(/what colour is a banana/);
     var spainCurrencyMatch = question.match(/what currency did Spain use before the Euro/);
     var minusMatch = question.match(/what is (\d+) minus (\d+)/);
+    var plusMatch = question.match(/what is (\d+) plus (\d+)/);
+    var doublePlusMatch = question.match(/what is (\d+) plus (\d+) plus (\d+)/);
     var eiffelMatch = question.match(/which city is the Eiffel tower in/);
     var pmMatch = question.match(/who is the Prime Minister of Great Britain/);
     if(largestMatch){
@@ -22,12 +25,25 @@ var answer = function(question, req, res) {
         return _.reduce(minusMatch.splice(1), function(number1, number2) {
             return number1 - number2;
         });
+    } else if (doublePlusMatch) {
+        return _.reduce(doublePlusMatch.splice(1), function(number1, number2) {
+            return number1 + number2;
+        });
+    } else if (plusMatch) {
+        return _.reduce(plusMatch.splice(1), function(number1, number2) {
+            return number1 + number2;
+        });
     } else if (eiffelMatch) {
         return 'Paris';
     } else if (pmMatch) {
         return 'David Cameron';
     }
 };
+
+function fibonnaci(number) {
+    if(number==1 || number ==2) return number;
+    fibonnaci(number)
+}
 
 var app = express();
 app.use(express.cookieParser());
@@ -39,7 +55,7 @@ app.get("/", function(req, res) {
     var q = req.param("q");
     var a = answer(q, req, res);
     console.log("Q: \"" + q + "\" A:\"" + a + "\"");
-    res.send(200, a);
+    res.end(a);
 });
 
 app.listen(8000, "localhost");
